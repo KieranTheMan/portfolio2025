6fab46a05cb241fdfdb8e6a6e0824e8096b273c4
KieranFowler <kieran9fowler@gmail.com>
2025-01-20 11:31:58 +0000

    imported and added 1-2-3 popUps to Portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,19 +1,48 @@
 import React, { useState, useEffect } from "react";
 import ContentImg from "../assets/images/content-img.png";
-import Popup from "./PopUp";
+import Popup1 from "./PopUp1";
+import Popup2 from "./PopUp2";
+import Popup3 from "./PopUp3";
 
 const Portfolio = () => {
-  const [isPopupVisible, setIsPopupVisible] = useState(false);
+ 
+  const [isPopup1Visible, setIsPopup1Visible] = useState(false);
+  const [isPopup2Visible, setIsPopup2Visible] = useState(false);
+  const [isPopup3Visible, setIsPopup3Visible] = useState(false);
 
-  const handlePopupOpen = (e) => {
-    e.preventDefault();
-    setIsPopupVisible(true);
-  };
 
-  const handlePopupClose = (e) => {
+ //<!-- ============= POPUP1=============== -->
+  const handlePopup1Open = (e) => {
     e.preventDefault();
-    setIsPopupVisible(false);
+    setIsPopup1Visible(true);
   };
+  const handlePopup1Close = (e) => {
+    e.preventDefault();
+    setIsPopup1Visible(false);
+  };
+  //<!-- ============= POPUP1-END=============== -->
+
+  //<!-- ============= POPUP2=============== -->
+  const handlePopup2Open = (e) => {
+    e.preventDefault();
+    setIsPopup2Visible(true);
+  };
+  const handlePopup2Close = (e) => {
+    e.preventDefault();
+    setIsPopup2Visible(false);
+  };
+  //<!-- ============= POPUP2-END=============== -->
+
+    //<!-- ============= POPUP3=============== -->
+    const handlePopup3Open = (e) => {
+      e.preventDefault();
+      setIsPopup3Visible(true);
+    };
+    const handlePopup3Close = (e) => {
+      e.preventDefault();
+      setIsPopup3Visible(false);
+    };
+    //<!-- ============= POPUP3-END=============== -->
 
   return (
     <>
@@ -29,13 +58,32 @@ const Portfolio = () => {
           <div>
             <div
               className="image-container popup-btn zoom_in"
-              onClick={handlePopupOpen}
+              onClick={handlePopup1Open}
             >
               <img src={ContentImg} alt="content-img" />
             </div>
-            <p className="Corporate zoom_in">AI IMAGE GEN APP</p>
+            <p className="Corporate zoom_in">AI Movie Selekt</p>
           </div>
           <div>
+            <div
+              className="image-container popup-btn zoom_in"
+              onClick={handlePopup2Open}
+            >
+              <img src={ContentImg} alt="content-img" />
+            </div>
+            <p className="Corporate zoom_in">AI Cool Art Gen</p>
+          </div>
+          <div>
+            <div
+              className="image-container popup-btn zoom_in"
+              onClick={handlePopup3Open}
+            >
+              <img src={ContentImg} alt="content-img" />
+            </div>
+            <p className="Corporate zoom_in">Rock Papper Scissors</p>
+          </div>
+          
+          {/* <div>
             <div
               className="image-container popup-btn zoom_in"
               onClick={handlePopupOpen}
@@ -43,19 +91,12 @@ const Portfolio = () => {
               <img src={ContentImg} alt="content-img" />
             </div>
             <p className="Corporate zoom_in">AI APP</p>
-          </div>
-          <div>
-            <div
-              className="image-container popup-btn zoom_in"
-              onClick={handlePopupOpen}
-            >
-              <img src={ContentImg} alt="content-img" />
-            </div>
-            <p className="Corporate zoom_in">AI APP</p>
-          </div>
+          </div> */}
         </div>
       </section>
-      <Popup isVisible={isPopupVisible} onClose={handlePopupClose} />
+      <Popup1 isVisible={isPopup1Visible} onClose={handlePopup1Close} />
+      <Popup2 isVisible={isPopup2Visible} onClose={handlePopup2Close} />
+      <Popup3 isVisible={isPopup3Visible} onClose={handlePopup3Close} />
       {/* <!-- ====================================== Section Portfolio End ===================================== --> */}
     </>
   );
